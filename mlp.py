fe397962b7ad5a55aa77e5b8a54993f19ed22f31
Emanuele Fasce <emanuele.fasce@sysdig.com>
2022-04-25 16:50:01 +0200

    refactor files titles and introduce pep

--- mlp.py
+++ mlp.py
@@ -4,20 +4,25 @@ import string
 import csv
 from keras.models import Sequential
 from keras.layers import Dropout, Dense
-from keras.optimizers import Adam
+from keras.optimizers import adam_v2
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
-    
+import os
+
+
+TRAINING_AND_VALIDATION_SAMPLES=28754
+
+
 def tokenizer(s):
     for punct in string.punctuation:
         s=s.replace(punct, " ")
     tags = tagger.tag_text(s)
     return [(x.split("\t"))[2] for x in tags if (x.split("\t"))[1]]
 
-#Reading files
-    
-train_file="development.csv"
-text_file="evaluation.csv"
+
+train_file=os.path.join('inputs', 'development.csv')
+test_file=os.path.join('inputs', 'evaluation.csv')
+
 
 reviews=[]
 grade=[]
@@ -34,51 +39,44 @@ with open(train_file,encoding="utf8") as csv_file:
            grade.append(0)
 y=grade
 
-with open(text_file,encoding="utf8") as csv_file:
+with open(test_file,encoding="utf8") as csv_file:
     reader=csv.reader(csv_file)
     next(reader)
     for row in reader:
         reviews.append(row[0])
         
 #Building the Tf-Idf matrix
-
 vectorizer=TfidfVectorizer(encoding='utf-8', strip_accents='unicode', lowercase=True,
                            tokenizer=tokenizer, stop_words=None, ngram_range=(1, 2))
 
 print("Starting X matrix fitting...")
 X=vectorizer.fit_transform(reviews)
 
-#Splitting into training test and test set
-
-Xtr=X[0:28754]
-Xte=X[28754:]
+#Splitting into training set and validation set
+X_train_and_valid=X[0:TRAINING_AND_VALIDATION_SAMPLES]
+X_test=X[TRAINING_AND_VALIDATION_SAMPLES:]
 
 #Building the model
-
 model2=Sequential()
-model2.add(Dense(input_dim=Xtr.shape[1], units=128, activation='relu'))
+model2.add(Dense(input_dim=X_train_and_valid.shape[1], units=128, activation='relu'))
 model2.add(Dropout(0.5))
 model2.add(Dense(units=64, activation='relu'))
 model2.add(Dropout(0.5))
 model2.add(Dense(units=1, activation='sigmoid'))
-model2.compile(loss='binary_crossentropy', metrics=['binary_accuracy'], optimizer=Adam())
+model2.compile(loss='binary_crossentropy', metrics=['binary_accuracy'], optimizer=adam_v2())
 callback=EarlyStopping(monitor='val_loss', patience=0)
 model2.summary()
 
 #Creating validation set
-
-XTrain, XTest, yTrain, yTest = train_test_split(Xtr, grade,test_size = 0.2)
+XTrain, XValid, yTrain, yValid = train_test_split(X_train_and_valid, grade, test_size = 0.2)
 
 #Fitting the model
-
-history = model2.fit(XTrain, yTrain, epochs=2, verbose=True, workers=-1, validation_data=(XTest, yTest), callbacks=[callback])
+history = model2.fit(XTrain, yTrain, epochs=2, verbose=True, workers=-1, validation_data=(XValid, yValid), callbacks=[callback])
 
 #Storing predictions into ypred
-
-ypred=model2.predict(Xte)
+ypred=model2.predict(X_test)
 
 #Transforming into binary predictions
-
 ypred2=[]
 for y in ypred:
     if abs(y-1)<abs(y):
@@ -87,10 +85,10 @@ for y in ypred:
         ypred2.append(0)
 
 #Saving to file
-
-with open('solutionsneural.csv', mode='w') as f:
+with open(os.path.join('outputs', 'solutionsneural.csv'), mode='w') as f:
     employee_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     employee_writer.writerow(['Id', 'Predicted'])
+
     for i,j in enumerate(ypred2):
         if j==1:
             employee_writer.writerow([i,"pos"])
--- rf-lstm.py
+++ rf-lstm.py
@@ -8,6 +8,11 @@ import treetaggerwrapper
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from sklearn.ensemble import RandomForestRegressor
+import os
+
+
+TRAINING_AND_VALIDATION_SAMPLES=28754
+
 
 def tokenizer(s):
     for punct in string.punctuation:
@@ -15,10 +20,10 @@ def tokenizer(s):
     tags = tagger.tag_text(s)
     return [(x.split("\t"))[2] for x in tags]
 
-#Reading files
 
-train_file="development.csv"
-text_file="evaluation.csv"
+train_file=os.path.join('inputs', 'development.csv')
+test_file=os.path.join('inputs', 'evaluation.csv')
+
 
 reviews=[]
 grade=[]
@@ -34,15 +39,15 @@ with open(train_file,encoding="utf8") as csv_file:
             grade.append(0)
 y=grade
 
-with open(text_file,encoding="utf8") as csv_file:
+with open(test_file,encoding="utf8") as csv_file:
     reader=csv.reader(csv_file)
     next(reader)
     for row in reader:
         reviews.append(row[0])
 
 #Stemming the reviews
-        
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='it')
+
 stemmed_reviews=[]
 i=0
 for r in reviews:
@@ -59,9 +64,7 @@ for r in reviews:
     print(f'{i}', end=' ')
     stemmed_reviews.append(rev)
 
-
 #Creating words dictionary
-
 index=0
 word_list={}
 for rev in stemmed_reviews:
@@ -72,7 +75,6 @@ for rev in stemmed_reviews:
             index+=1
 
 #Embedding reviews into arrays
-            
 reviews_dic=[]
 for rev in stemmed_reviews:
     rev_arr=[]
@@ -84,7 +86,6 @@ for rev in stemmed_reviews:
     reviews_dic.append(rev_arr)
 
 #Padding reviews with zeros
-    
 reviews_pad=[]
 for arr in reviews_dic:
     myarr=np.array(arr)
@@ -93,22 +94,18 @@ for arr in reviews_dic:
     reviews_pad.append(zero)
 
 #Creating X matrix with the arrays
-
 matX=np.zeros(shape=(len(reviews_dic), 1700))
 for i in range(len(reviews_dic)):
     matX[i,:]=reviews_pad[i].astype(int)
     
 #Splitting into training set and test set
-    
-trainX=matX[0:28754].astype(int)
-testX=matX[28754:].astype(int)
+trainX=matX[0:TRAINING_AND_VALIDATION_SAMPLES].astype(int)
+testX=matX[TRAINING_AND_VALIDATION_SAMPLES:].astype(int)
 
 #Building validation set
-
 XTrain, XVal, yTrain, yVal = train_test_split(trainX, grade, test_size = 0.2)
 
-#Building Bidirectional LSTM 
-
+#Building Bidirectional LSTM
 batch_size=64
 max_feat=len(word_list)
 model = Sequential()
@@ -120,34 +117,28 @@ model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
 model.fit(XTrain, yTrain,batch_size=batch_size, epochs=1, validation_data=(XVal, yVal))
 ypred=model.predict(testX)
 
-#Now all the results of the RNN are stored in ypred
 
 #Now I build the Random Forest Regressor, starting with the Tf-Idf matrix
-
 vectorizer=TfidfVectorizer(encoding='utf-8', strip_accents='unicode', lowercase=True,
                            tokenizer=tokenizer, stop_words=None)
 
 X=vectorizer.fit_transform(reviews)
 
 #Splitting into training set and test set
-
-trX=X[0:28754]
-teX=X[28754:]
+trX=X[0:TRAINING_AND_VALIDATION_SAMPLES]
+teX=X[TRAINING_AND_VALIDATION_SAMPLES:]
 
 #Building the random forest regressor model and predicting the results
-
 rfr=RandomForestRegressor(n_estimators=200)
 rfr.fit(trX, grade)
 rfrpred=rfr.predict(teX)
 
 #Storing the sum of the results into ypred_combined
-
 ypred_combined=[]
 for i, j in enumerate(rfrpred):
     ypred_combined.append(j+ypred[i])
 
 #Storing the binary predictions into yfinal, the cutoff is 1
-    
 yfinal=[]
 for y in ypred_combined:
     if y>1:
@@ -156,8 +147,7 @@ for y in ypred_combined:
         yfinal.append(0)
     
 #Saving to file
-
-with open('solutions_comb.csv', mode='w') as f:
+with open(os.path.join('inputs', 'predictions_rf-lstm.csv'), mode='w') as f:
     employee_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     employee_writer.writerow(['Id', 'Predicted'])
     for i,j in enumerate(yfinal):
